663c90c00c1c8be00c73cc7012f6aee38a5c806a
Raj-101206 <rrajasekar101206@gmail.com>
2026-07-08 17:30:15 +0530

    Initial backend setup

---
+++ src/main/java/com/matchdayai/MatchdayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.matchdayai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchdayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchdayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matchdayai/matchday_backend/MatchdayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchdayai.matchday_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchdayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
